512120ab186eac300e6e731126b2b9cb50bae90d
Pujitha-Akurathi <pujitha.akurathi05@gmail.com>
2025-08-05 23:55:23 +0530

    Update footer.tsx

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -80,6 +80,7 @@ const Footer = () => {
       className="w-full sm:w-64 px-4 py-2 rounded-lg bg-white/10 border border-white/20 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-white/30"
     />
     </div>
+  <br></br>
     <button className="px-6 py-2 bg-white text-primary rounded-lg font-medium hover:bg-white/90 transition-colors duration-200">
       Subscribe
     </button>
